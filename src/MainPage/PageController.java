c07723c127bbd6c104ab50fe5a16d0782baaa135
pasindulak <hgdpasindu@gmail.com>
2020-09-02 02:12:45 +0530

    added drag method

--- src/MainPage/PageController.java
+++ src/MainPage/PageController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -15,10 +16,12 @@ public class PageController implements Initializable {
     Button encrypt;
     @FXML
     Button decrypt;
+    @FXML
+    HBox header;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        dragOn();
     }
 
     @FXML
@@ -28,5 +31,20 @@ public class PageController implements Initializable {
     @FXML
     private void shutdown(MouseEvent e){
         ((Stage) encrypt.getScene().getWindow()).close();
+    }
+
+             /**----method for drag feature to the window------------*/
+        double x;
+        double y;
+    private void dragOn(){
+        header.setOnMousePressed(event -> {
+           x= event.getSceneX();
+           y = event.getSceneY();
+        });
+        header.setOnMouseDragged(event -> {
+           Stage stage = (Stage) header.getScene().getWindow();
+           stage.setX(event.getScreenX()-x);
+           stage.setY(event.getScreenY()-y);
+        });
     }
 }

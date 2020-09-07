6a95f4e9b21d3e3db1e053dd1f34691d1491b35e
pasindulak <hgdpasindu@gmail.com>
2020-09-08 01:15:39 +0530

    Build AESEngine #1

--- src/aes/AESEngine.java
+++ src/aes/AESEngine.java
@@ -1,6 +1,8 @@
 package aes;
 
+import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
 import java.security.Key;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -8,21 +10,64 @@ import java.util.Arrays;
 
 public class AESEngine {
     private SecretKeySpec key;
-    private boolean file ; // true for file and false for folder
+    private int mode;
 
-    public AESEngine(String key, String hash, int keySize, boolean file) {   // keySize should be in byte
-        MessageDigest messageDigest=null;
+
+    public AESEngine(String key, String hash, int keySize, int mode) {   // keySize should be in byte
+        MessageDigest messageDigest = null;
         try {
             messageDigest = MessageDigest.getInstance(hash);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-          this.key = keygen(Arrays.copyOf(messageDigest.digest(key.getBytes()),keySize));
+        this.key = keygen(Arrays.copyOf(messageDigest.digest(key.getBytes()), keySize));
 
-        this.file = file;
+
+        this.mode = mode;
     }
 
-    private SecretKeySpec keygen(byte [] arr){
-       return new SecretKeySpec(arr , "AES");
+    private SecretKeySpec keygen(byte[] arr) {
+        return new SecretKeySpec(arr, "AES");
     }
+
+    public boolean crypt(File inputFile){
+        try {
+            if (inputFile.isDirectory())
+                recuCryption(inputFile);
+            else
+                doCrypt(inputFile);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    private void recuCryption(File inputFile){
+        for (File file: inputFile.listFiles()) {
+            if(file.isDirectory()) {
+                recuCryption(file);
+                continue;
+            }
+            doCrypt(file);
+        }
+    }
+
+    private void doCrypt(File file){
+
+     try {
+     byte [] data = new byte[(int)file.length()];
+         FileInputStream inputStream = new FileInputStream(file);
+         inputStream.read(data);
+         inputStream.close();
+         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+         cipher.init(mode,key);
+         byte [] temp = cipher.doFinal(data);
+         FileOutputStream outputStream = new FileOutputStream(file);
+         outputStream.write(temp);
+         outputStream.close();
+     } catch (Exception e) {
+         e.printStackTrace();
+     }
+    }
+
 }

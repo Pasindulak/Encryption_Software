451acc8e921470e7dd385ac207d09e77a9e70eea
pasindulak <hgdpasindu@gmail.com>
2020-10-14 12:39:26 +0530

    added MetaBuild class and built contructor

---
+++ src/aes/MetaBuild.java
@@ -0,0 +1,10 @@
+package aes;
+
+public class MetaBuild extends AESEngine{
+
+    public MetaBuild(String key, String hash, int keySize, int mode,boolean save) {
+        super(key, hash, keySize, mode);
+    }
+
+
+}

9c1e777b3239d7442d8c9adf5200a41677bb7882
pasindulak <hgdpasindu@gmail.com>
2020-10-15 17:44:34 +0530

    remove metaBuild class cause to improve security

--- src/aes/AESEngine.java
+++ src/aes/AESEngine.java
@@ -71,4 +71,26 @@ public class AESEngine {
         }
     }
 
+    /**   ---------rules for meta data adding------------
+     * If first 5 bytes of the file represent "saved" there should be meta data for decrypting
+     * else get key size and hashing algorithm from the user.
+     *
+     * After "saved" there should be two bytes of data. first one for key size and
+     * second one for hashing algorithm
+     *
+     * For key size
+     *      0 - 128 bits
+     *      1 - 192 bits
+     *      2 - 256 bits
+     *
+     * For Hashing algo
+     *      0 - MD5
+     *      1 - SHA-1
+     *      2 - SHA-256
+     *      3 - SHA-384
+     *      4 - SHA-512
+     *
+     * */
+
+
 }
